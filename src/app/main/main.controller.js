055312bc8c80d29921bc9efffdc4ef5a28b714af
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-05-21 11:52:36 -0400

    removed constructor references

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,12 +1,6 @@
 export class MainController {
-  constructor ($timeout, webDevTec, toastr) {
+  constructor () {
     'ngInject';
 
-    this.awesomeThings = [];
-    this.classAnimation = '';
-    this.creationDate = 1495377287609;
-    this.toastr = toastr;
-
-    this.activate($timeout, webDevTec);
   }
 }
