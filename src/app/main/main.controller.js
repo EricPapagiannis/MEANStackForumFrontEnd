d7e33400d3710bacf7aef92761868ecbc80bf5c9
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-05-21 15:20:17 -0400

    Created ui elements to display all messages, and modified index js to accomplish that

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,7 +3,16 @@ export class MainController {
     'ngInject';
 
     this.$http = $http;
+    this.getMessages();
   }
+
+  getMessages() {
+    var vm = this;
+    this.$http.get('http://localhost:5000/api/message').then(function(result){
+      vm.messages = result.data;
+    });
+  }
+
   postMessage() {
     this.$http.post('http://localhost:5000/api/message', {msg: this.message});
   }
