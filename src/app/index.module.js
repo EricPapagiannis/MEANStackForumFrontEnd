f6e2fa6d65331d725644a85257bd038ebf638170
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-05-21 15:43:22 -0400

    Routed auth page

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,10 +4,12 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
+import { AuthController } from './auth/auth.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
+
 
 angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr'])
   .constant('malarkey', malarkey)
@@ -18,5 +20,6 @@ angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr'])
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
+  .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,6 +6,13 @@ export function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+
+    .state('auth', {
+      url: '/auth',
+      templateUrl: 'app/auth/auth.html',
+      controller: 'AuthController',
+      controllerAs: 'auth'
     });
 
   $urlRouterProvider.otherwise('/');
