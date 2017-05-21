3ea92a75ee69ddaf911897b1447fd6f53eb58ea5
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-05-21 18:46:30 -0400

    Created logout functionality

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,6 +5,7 @@ import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { AuthController } from './auth/auth.controller';
+import { NavbarController } from './components/navbar/navbar.controller';
 import { CompareToDirective } from './directives/compareTo.directive';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
@@ -23,6 +24,7 @@ angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr', 'satell
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
   .controller('AuthController', AuthController)
+  .controller('NavbarController', NavbarController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective)
   .directive('compareTo', CompareToDirective);
