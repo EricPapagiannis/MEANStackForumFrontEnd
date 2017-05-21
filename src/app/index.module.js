bb959592a0140b0a8ee8796750dae6531411d74f
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-05-21 16:15:50 -0400

    Added password comparison

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,6 +5,7 @@ import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { AuthController } from './auth/auth.controller';
+import { CompareToDirective } from './directives/compareTo.directive';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
@@ -22,4 +23,5 @@ angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr'])
   .controller('MainController', MainController)
   .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
+  .directive('acmeMalarkey', MalarkeyDirective)
+  .directive('compareTo', CompareToDirective);
