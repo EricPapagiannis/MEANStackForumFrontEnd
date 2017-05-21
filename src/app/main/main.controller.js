39fc8d0e90301a6814a47a8e3d6305d615f52d3b
Eric Papagiannis <eric.papagiannis@mail.utoronto.ca>
2017-05-21 11:52:11 -0400

    Removed starter code

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,24 +9,4 @@ export class MainController {
 
     this.activate($timeout, webDevTec);
   }
-
-  activate($timeout, webDevTec) {
-    this.getWebDevTec(webDevTec);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
-  }
-
-  getWebDevTec(webDevTec) {
-    this.awesomeThings = webDevTec.getTec();
-
-    angular.forEach(this.awesomeThings, (awesomeThing) => {
-      awesomeThing.rank = Math.random();
-    });
-  }
-
-  showToastr() {
-    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    this.classAnimation = '';
-  }
 }
